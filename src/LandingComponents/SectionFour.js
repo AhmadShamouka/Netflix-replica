fbd4d81956e812408c3d46152774a5d73c7225ff
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-11-19 17:02:33 +0200

    add section four component

---
+++ src/LandingComponents/SectionFour.js
@@ -0,0 +1,20 @@
+// SectionFour.js
+import React from 'react';
+import './Landing.css'
+
+const SectionFour = () => {
+  return (
+    <div className="feature">
+      <div className="feature__details">
+        <h3 className="feature__title">Create profiles for Kids</h3>
+        <h5 className="feature__sub__title">
+        Send kids on adventures with their favorite characters in 
+        a space made just for them—free with your membership.
+        </h5>
+      </div>
+     
+    </div>
+  );
+};
+
+export default SectionFour;
