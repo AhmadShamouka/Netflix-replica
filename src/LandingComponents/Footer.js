68fada9e800eb200cdb066bf7ad8bc610ea6b3f8
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-11-19 17:45:57 +0200

    add Footer rows

--- src/LandingComponents/Footer.js
+++ src/LandingComponents/Footer.js
@@ -4,7 +4,18 @@ import './Landing.css'
 const Footer = () => {
   return (
     <footer>
-     
+      <div className="footer__row__1">
+        <h4>Questions? Contact us.</h4>
+      </div>
+      <div className="footer__row__2">
+    
+      </div>
+      <div className="footer__row__3">
+       
+      </div>
+      <div className="footer__row__4">
+    
+      </div>
     </footer>
   );
 };
