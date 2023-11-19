b17a5d31777ec8c1536158b421c931e51c81f733
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-11-19 17:47:21 +0200

    add Footer columns

--- src/LandingComponents/Footer.js
+++ src/LandingComponents/Footer.js
@@ -8,13 +8,35 @@ const Footer = () => {
         <h4>Questions? Contact us.</h4>
       </div>
       <div className="footer__row__2">
-    
+        <div className="column__1">
+          <p>FAQ</p>
+          <p>Investor Relations</p>
+          <p>Privacy</p>
+          <p>Speed Test</p>
+        </div>
+        <div className="column__2">
+          <p>Help Center</p>
+          <p>Jobs</p>
+          <p>Cookie Preferences</p>
+          <p>Legal Notices</p>
+        </div>
+        <div className="column__3">
+          <p>Account</p>
+          <p>Ways to Watch</p>
+          <p>Corporate Information</p>
+          <p>Only on Netflix</p>
+        </div>
+        <div className="column__4">
+          <p>Media Centre</p>
+          <p>Terms of Use</p>
+          <p>Contact Us</p>
+        </div>
       </div>
       <div className="footer__row__3">
        
       </div>
       <div className="footer__row__4">
-    
+        <p>Netflix India</p>
       </div>
     </footer>
   );
