db50980f4c9d17e9c216e4b46492374500e2536d
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-11-19 17:03:23 +0200

    add div image container section four

--- src/LandingComponents/SectionFour.js
+++ src/LandingComponents/SectionFour.js
@@ -12,7 +12,13 @@ const SectionFour = () => {
         a space made just for them—free with your membership.
         </h5>
       </div>
-     
+      <div className="feature__image__container">
+        <img
+          src="https://occ-0-4023-2164.1.nflxso.net/dnm/api/v6/19OhWN2dO19C9txTON9tvTFtefw/AAAABVxdX2WnFSp49eXb1do0euaj-F8upNImjofE77XStKhf5kUHG94DPlTiGYqPeYNtiox-82NWEK0Ls3CnLe3WWClGdiJP.png?r=5cf"
+          alt="Feature image"
+          className="feature__image"
+        />
+      </div>
     </div>
   );
 };
