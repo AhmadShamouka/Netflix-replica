6282a91aea4d88774975951a4c4a2d329d530c04
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-11-19 16:37:48 +0200

    add section one and section two

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
+import './LandingComponents/Landing.css'
 import Header from './LandingComponents/Header';
 import Hero from './LandingComponents/Hero';
+import SectionOne from './LandingComponents/SectionOne';
+import SectionTwo from './LandingComponents/SectionTwo';
 
 function App() {
   return (
     <div className="App">
     <Header/>
     <Hero/>
+    <SectionOne/>
+    <SectionTwo/>
+    {/* <SectionThree/>
+    <SectionFour/> */}
     </div>
   );
 }
