3491c24acce37e1393741c71698eddba5c22ef67
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-11-21 01:09:44 +0200

    media query

--- src/LandingComponents/Footer.js
+++ src/LandingComponents/Footer.js
@@ -1,52 +1,60 @@
-import React from 'react';
-import './Landing.css'
+import React from "react";
+import "./Landing.css";
 
 const Footer = () => {
   return (
     <footer>
-      <div className="footer__row__1">
+      <div className="footer__q">
         <h4>Questions? Contact us.</h4>
       </div>
-      <div className="footer__row__2">
-        <div className="column__1">
-          <p>FAQ</p>
-          <p>Investor Relations</p>
-          <p>Privacy</p>
-          <p>Speed Test</p>
+      <div className="footer_main">
+        <div className="footer-column">
+          <div className="footer__row">
+            <div className="column__1">
+              <a>FAQ</a>
+              <a>Investor Relations</a>
+              <a>Privacy</a>
+              <a>Speed Test</a>
+            </div>
+            <div className="column__2">
+              <a>Help Center</a>
+              <a>Jobs</a>
+              <a>Cookie Preferences</a>
+              <a>Legal Notices</a>
+            </div>
+            <div className="column__3">
+              <a>Account</a>
+              <a>Ways to Watch</a>
+              <a>Corporate Information</a>
+              <a>Only on Netflix</a>
+            </div>
+            <div className="column__4">
+              <a>Media Centre</a>
+              <a>Terms of Use</a>
+              <a>Contact Us</a>
+            </div>
+          </div>
         </div>
-        <div className="column__2">
-          <p>Help Center</p>
-          <p>Jobs</p>
-          <p>Cookie Preferences</p>
-          <p>Legal Notices</p>
-        </div>
-        <div className="column__3">
-          <p>Account</p>
-          <p>Ways to Watch</p>
-          <p>Corporate Information</p>
-          <p>Only on Netflix</p>
-        </div>
-        <div className="column__4">
-          <p>Media Centre</p>
-          <p>Terms of Use</p>
-          <p>Contact Us</p>
-        </div>
+        
       </div>
-      <div className="footer__row__3">
-      <div className="dropdown__container">
+
+      <div className="footer__row">
+        <div className="dropdown__container">
           <i className="fas fa-globe"></i>
           <select
             name="languages"
             id="languagesSelect"
             className="language__drop__down"
           >
-            <option value="english" selected>English</option>
+            <option value="english" selected>
+              English
+            </option>
             <option value="Arabic">العربية</option>
           </select>
         </div>
       </div>
-      <div className="footer__row__4">
-        <p>Netflix Lebanon</p>
+      <div className="footer__row">
+        <a>Netflix Lebanon</a>
       </div>
     </footer>
   );
--- src/LandingComponents/Header.js
+++ src/LandingComponents/Header.js
@@ -17,7 +17,7 @@ const Header = () => {
   
           <div className="navbar__nav__items">
             <div className="nav__item">
-              <div className="dropdown__container">
+              <div className='item_select'>
                 <i className="fas fa-globe"></i>
                 <select
                   name="languages"
@@ -32,11 +32,11 @@ const Header = () => {
                   </option>
                
                 </select>
-              </div>
             </div>
+         </div>
   
             <div className="nav__item">
-              <button className="signin__button">Sign in</button>
+              <a className="signin__button">Sign in</a>
             </div>
           </div>
         </nav>
